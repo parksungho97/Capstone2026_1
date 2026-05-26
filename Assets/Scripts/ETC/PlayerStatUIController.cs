429ca9ae98afaada7d3137032926f0b47f79fc18
JiHunJeong <wjdwlgns8096@naver.com>
2026-05-26 16:11:59 +0900

    Save

--- Assets/Scripts/ETC/PlayerStatUIController.cs
+++ Assets/Scripts/ETC/PlayerStatUIController.cs
@@ -10,38 +10,25 @@ public class PlayerStatUIController : MonoBehaviour
     [SerializeField] private StatVisibleUI hpVisibleUI;
     [SerializeField] private StatVisibleUI armorVisibleUI;
 
+    private void Start()
+    {
+        Debug.Assert(hpVisibleUI);
+        Debug.Assert(armorVisibleUI);
+    }
+
     public void Initialize(CharacterHealth playerHealth)
     {
         this.playerHealth = playerHealth;
         Debug.Assert(this.playerHealth);
     }
+
     private void Update()
     {
         if (playerHealth == null)
-        {
-            CharacterHealth[] all = FindObjectsOfType<CharacterHealth>();
-
-            foreach (var ph in all)
-            {
-                if (ph != null && ph.Object != null && ph.Object.IsValid)
-                {
-                    playerHealth = ph;
-                    Debug.Log("[UI] Spawn된 PlayerHealth 자동 연결 완료");
-                    break;
-                }
-            }
-
-            if (playerHealth == null)
-                return;
-        }
-
-        if (playerHealth.Object == null || !playerHealth.Object.IsValid)
             return;
 
-        if (hpVisibleUI != null)
-            hpVisibleUI.VisibleState(playerHealth.CurrentHP, playerHealth.MaxHP);
+        hpVisibleUI.VisibleState(playerHealth.CurrentHP, playerHealth.MaxHP);
 
-        if (armorVisibleUI != null)
-            armorVisibleUI.VisibleState(playerHealth.CurrentArmor, playerHealth.MaxArmor);
+        armorVisibleUI.VisibleState(playerHealth.CurrentArmor, playerHealth.MaxArmor);
     }
 }
